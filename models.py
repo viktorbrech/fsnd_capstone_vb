0fc089227bb606fadd79f7b7c298ebc74067d290
viktorbrech <viktor.brech@gmail.com>
2020-12-02 00:36:25 +0100

    Remove drop_all from setup_db

--- models.py
+++ models.py
@@ -16,8 +16,6 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    # add drop_all for testing
-    db.drop_all()
     db.create_all()
 
 
