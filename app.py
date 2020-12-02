6b38eb2559a63e4812eb4b0c601a89f2fce149cb
viktorbrech <viktor.brech@gmail.com>
2020-12-03 00:58:59 +0100

    Add untested POST endpoints and PATCH/DELETE commented templates

--- app.py
+++ app.py
@@ -15,28 +15,24 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app)
 
+    # mini-frontend "Home", redirects to login page
     @app.route('/')
     def get_greeting():
-        excited = os.environ['EXCITED']
-        greeting = "Hello" 
-        if excited == 'true': greeting = greeting + "!!!!!"
-        return greeting
+        return redirect(url_for('login_page'))
 
+    # mini-frontend "Login" page
     @app.route('/login')
     def login_page():
         return render_template("login.html")
 
+    # mini-frontend "Callback" page, Auth0 redirects to here with access_token
     @app.route('/callback')
     def callback_page():
         return render_template("callback.html")
 
-    @app.route('/coolkids')
-    def be_cool():
-        return "Be cool, man, be coooool! You're almost a FSND grad!"
-
     # GET /actors
     @app.route('/actors')
-    # @requires_auth(permission="get:actors")
+    @requires_auth(permission="get:actors")
     def get_actors():
         all_actors = Actor.query.all()
         actors = []
@@ -55,15 +51,61 @@ def create_app(test_config=None):
         return jsonify({"success": True, "movies": movies})
 
     # DELETE /actors/id
+    
+    # @app.route('/drinks/<int:drink_id>', methods=['DELETE'])
+    # @requires_auth(permission="delete:drinks")
+    # def delete_drinks(payload, drink_id):
+    #     drink = Drink.query.get(drink_id)
+    #     if drink:
+    #         drink.delete()
+    #     else:
+    #         abort(404)
+    #     return jsonify({'success': True, 'delete': drink_id})
 
     # DELETE /movies/id
 
     # POST /actors
+    @app.route('/actors', methods=['POST'])
+    @requires_auth(permission="post:actors")
+    def post_actors(payload):
+        data = request.get_json()
+        # TODO ensure proper type conversions inside the following constructor
+        new_actor = Actor(
+            name=data["name"],
+            age=data["age"],
+            gender=data["gender"])
+        new_actor.insert()
+        return jsonify({'success': True, 'actors': [new_actor.format()]})
 
     # POST /movies
+    @app.route('/movies', methods=['POST'])
+    @requires_auth(permission="post:movies")
+    def post_movies(payload):
+        data = request.get_json()
+        # TODO ensure proper type conversions inside the following constructor
+        new_movie = Movie(
+            title=data["title"],
+            release_date=data["release_date"],
+        new_movie.insert()
+        return jsonify({'success': True, 'movies': [new_movie.format()]})
 
     # PATCH /actors/id
 
+    # @app.route('/drinks/<int:drink_id>', methods=['PATCH'])
+    # @requires_auth(permission="patch:drinks")
+    # def patch_drinks(payload, drink_id):
+    #     data = request.get_json()
+    #     drink = Drink.query.get(drink_id)
+    #     if "title" in data:
+    #         drink.title = data["title"]
+    #     if "recipe" in data:
+    #         if isinstance(data["recipe"], list):
+    #             drink.recipe = json.dumps(data["recipe"])
+    #         else:
+    #             drink.recipe = json.dumps([data["recipe"]])
+    #     drink.update()
+    #     return jsonify({'success': True, 'drinks': [drink.long()]})
+    
     # PATCH /movies/id
 
     """
