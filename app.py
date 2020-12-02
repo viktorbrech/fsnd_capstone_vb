84ba9cb6f06c12a8c4189ef8fad73b24a306eb32
viktorbrech <viktor.brech@gmail.com>
2020-12-02 11:38:57 +0100

    Add error handlers

--- app.py
+++ app.py
@@ -54,6 +54,44 @@ def create_app(test_config=None):
 
     # PATCH /movies/id
 
+    """
+    ERROR HANDLERS TAKEN FROM MY COFFEE_SHOP PROJECT SUBMISSION
+    """
+    # Error Handling
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+                        "success": False,
+                        "error": 422,
+                        "message": "unprocessable"
+                        }), 422
+
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+                        "success": False,
+                        "error": 400,
+                        "message": "unprocessable"
+                        }), 400
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+                        "success": False,
+                        "error": 404,
+                        "message": "resource not found"
+                        }), 404
+
+
+    @app.errorhandler(AuthError)
+    def auth_error(error):
+        return jsonify({
+                        "success": False,
+                        "error": error.status_code,
+                        "message": error.error
+                        }), error.status_code
     return app
 
 app = create_app()
