66da1685591b65debaad85c59a4cdeb0d328784e
viktorbrech <viktor.brech@gmail.com>
2020-12-03 22:06:22 +0100

    Add final endpoints, fixed model formatting, corrected type conversions

--- setup.sh
+++ setup.sh
@@ -1,2 +1,1 @@
-export DATABASE_URL=postgres://lardgjqncqfvak:a1e65623bbbbf17fedf77c1f4862dc1075646741f36fd2785a8914b7dada4bae@ec2-174-129-199-54.compute-1.amazonaws.com:5432/d7k1gckgtsvfd
 export EXCITED=false
